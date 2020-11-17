7899b566bb2a7398491c3422657c8bd6bcdd1f55
gabbie amah <gabbieamah49@gmail.com>
2020-11-17 20:52:29 +0100

    handled promise rejection

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGO_URL, { useUnifiedTopology: true, useNewUrlParser: true })
-server.listen(port, () => {
-  console.log(`this server is running on ${port}`);
-});
+  .then(() => server.listen(port, () => {
+    console.log(`this server is running on ${port}`);
+  })
+  )
+  .catch((err) => {
+    console.log(err)
+  })
